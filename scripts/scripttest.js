95239fec62c1084763549bc1639253a9e7998e36
jdecampos <contact@jdc-marketing.fr>
2024-03-30 14:51:11 +0100

    Create scripttest.js

---
+++ scripts/scripttest.js
@@ -0,0 +1,11 @@
+  GNU nano 6.2                                                                                                                            script1.js                                                                                                                                      
+const puppeteer = require('puppeteer');
+
+exports.run = async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://nocodeskills.fr');
+  // Votre logique de scraping ou autre ici
+  await browser.close();
+  res.send('Script 1 exécuté avec succès');
+};
